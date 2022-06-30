52917e9b2fa0465e4b6f3721fc15a35d0a6390de
Hein du Plessis <duplessish@gmail.com>
2022-06-30 16:15:39 +0200

    Added front end validations

--- ChessClub.API/Models/AddMemberRequest.cs
+++ ChessClub.API/Models/AddMemberRequest.cs
@@ -1,19 +1,26 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ChessClub.API.Models
 {
     public class AddMemberRequest
     {
         [JsonPropertyName("name")]
+        [Required]
         public string Name { get; set; } = string.Empty;
 
         [JsonPropertyName("surname")]
+        [Required]
         public string Surname { get; set; } = string.Empty;
 
         [JsonPropertyName("email")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
         [JsonPropertyName("birthday")]
+        [Required]
         public DateTime Birthday { get; set; } = default;
     }
 }
--- ChessClub.API/Models/MemberDTO.cs
+++ ChessClub.API/Models/MemberDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ChessClub.API.Models
 {
@@ -8,15 +9,21 @@ namespace ChessClub.API.Models
         public Guid Id { get; set; } = default;
 
         [JsonPropertyName(name: "name")]
+        [Required]
         public string Name { get; set; } = string.Empty;
 
         [JsonPropertyName("surname")]
+        [Required]
         public string Surname { get; set; } = string.Empty;
 
         [JsonPropertyName("email")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
         [JsonPropertyName("birthday")]
+        [Required]
         public DateTime Birthday { get; set; } = default;
 
         [JsonPropertyName("current-rank")]
